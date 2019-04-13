0cba6e9df565aef95c3979ca652c7c99d7be2ac7
Ricardo <rkrdo.a.v@gmail.com>
2019-04-13 15:02:55 -0600

    added code to loop in producer's CS

--- producer/producer_main.c
+++ producer/producer_main.c
@@ -1,9 +1,10 @@
-#include <sys/ipc.h> 
-#include <sys/shm.h> 
-#include <stdio.h> 
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "utilities/time_util.h"
 #include "data_structures/shared_memory.h"
@@ -14,24 +15,28 @@ message *msg;
 int main(int argc, char **argv)
 {
     //get the id from argv
-    int shmid = atoi(&(*argv[1])); 
-  
-    // shmat to attach to shared memory 
-    sm_ptr = (shared_memory*) shmat(shmid, 0,0);
-    sm_ptr->buffer = (circular_buffer*)shmat(sm_ptr->cb_shmid, NULL, 0);
-    (*sm_ptr->buffer).messages = (message*)shmat(sm_ptr->m_shmid, NULL, 0);
-  
+    int shmid = atoi(&(*argv[1]));
+
+    // shmat to attach to shared memory
+    sm_ptr = (shared_memory *)shmat(shmid, 0, 0);
+    sm_ptr->buffer = (circular_buffer *)shmat(sm_ptr->cb_shmid, NULL, 0);
+    (*sm_ptr->buffer).messages = (message *)shmat(sm_ptr->m_shmid, NULL, 0);
+
     sm_ptr->producers_count++;
     int pid = getpid();
-    char *date_time = get_current_date();
 
-    printf("%d\n", sm_ptr->producers_count);
-    printf("PID = %d\n", pid);
+    while (1)
+    {
+        usleep(10000000);
+        char *date_time = get_current_date();
+        printf("%d\n", sm_ptr->producers_count);
+        printf("PID = %d\n", pid);
 
-    CB_push(sm_ptr->buffer, pid, 1);
-      
-    //detach from shared memory  
-    shmdt(sm_ptr); 
-     
+        CB_push(sm_ptr->buffer, pid, 1);
+    }
+
+    //detach from shared memory
+    shmdt(sm_ptr);
+
     return 0;
 }
