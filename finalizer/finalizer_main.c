1a0207d211f184b566fd795fb3349992c6703ff8
rjimenez167 <rjimenez167@gmail.com>
2019-04-21 22:39:51 -0600

    Add files via upload

--- finalizer/finalizer_main.c
+++ finalizer/finalizer_main.c
@@ -11,10 +11,11 @@
 #include "../data_structures/shared_memory.h"
 #include "../data_structures/circular_buffer.h"
 
-
 shared_memory *sm_ptr;
 message *msg;
 crono Cronometers;
+int total_consumers = 0;
+int total_producers = 0;
 
 int main(int argc, char **argv)
 {
@@ -30,37 +31,39 @@ int main(int argc, char **argv)
     //get the id from argv
     int shmid = atoi(&(*argv[1])); //TODO change this to index 0
 
-    if(shmid < 1){
+    if (shmid < 1)
+    {
         fprintf(stderr, usage_msj, "All values must be a positive number greater than zero!\n");
         return EXIT_FAILURE;
     }
 
-
-
-  
-    // shmat to attach to shared memory 
-    sm_ptr = (shared_memory*) shmat(shmid, 0,0);
+    // shmat to attach to shared memory
+    sm_ptr = (shared_memory *)shmat(shmid, 0, 0);
 
     // semaphores configuration
-    struct sembuf sb  = {0, -1, 0};
+    struct sembuf sb = {0, -1, 0};
     struct sembuf sb1 = {1, 1, 0};
     struct sembuf sb2 = {2, -1, 0};
+
+    int total_consumers = sm_ptr->consumers_count;
+    int total_producers = sm_ptr->producers_count;
+    int total_messages_finalizer = 0;
 
     printf("Please input something to kill producers: \n");
     scanf("%d \n", &user_input);
     printf("You entered: %d\n", user_input);
 
-    (sm_ptr->suspend_producers)=true;
+    (sm_ptr->suspend_producers) = true;
 
     printf("Producers should be finalizing by now \n");
-    
+
     int pid = getpid();
-    while (sm_ptr->consumers_count>0) // this condition should be changed so the loop goes until the producer is said to no longer produce
+
+    while (sm_ptr->consumers_count > 0) // this condition should be changed so the loop goes until the producer is said to no longer produce
     {
 
         //producer is going to wait a exp_delay that is a random number calculated from exponential distribution
         usleep(100000);
-
 
         //timestamp from the instant when request to shared memory starts
         cronometer_start(&Cronometers.start_waiting_time);
@@ -69,25 +72,31 @@ int main(int argc, char **argv)
         //timestamp from the instant when request to shared memory starts
         cronometer_end(&Cronometers.end_waiting_time);
         save_time_spent(&Cronometers.start_waiting_time, &Cronometers.end_waiting_time, &Cronometers.time_spent_waiting_shared_memory);
-        
 
         // attach buffer and messages structures to the shared memory
         sm_ptr->buffer = (circular_buffer *)shmat(sm_ptr->cb_shmid, NULL, 0);
         (*sm_ptr->buffer).messages = (message *)shmat(sm_ptr->m_shmid, NULL, 0);
-        
+
         sm_ptr->total_messages++;
         // append message to the buffer
-        CB_push(sm_ptr->buffer, pid, 6,sm_ptr->consumers_count,sm_ptr->producers_count,sm_ptr->total_messages);
-
-
+        CB_push(sm_ptr->buffer, pid, 6, sm_ptr->consumers_count, sm_ptr->producers_count, sm_ptr->total_messages);
+        total_messages_finalizer++;
 
         sb.sem_op = 1;
         semop(sm_ptr->semid, &sb, 1);
         semop(sm_ptr->semid, &sb1, 1); // prevents underflow
     }
+    printf("===============================================================\n");
+    printf("=== Report of Finalizer with id: %5d                      ===\n", pid);
+    printf("=== Number of sent messages    : %5d                      ===\n", total_messages_finalizer);
+    printf("=== Total number of sent messages    : %5d                ===\n", sm_ptr->total_messages);
+    printf("=== Total execution time is: %5d (s)                      ===\n", Cronometers.execution_time);
+    printf("=== Total of finalized consumers: %5d                     ===\n", total_consumers);
+    printf("=== total of finalized producers: %5d                     ===\n", total_producers);
+    printf("===============================================================\n");
 
-    // destroy the shared memory 
-    shmctl(shmid,IPC_RMID,NULL); 
+        // destroy the shared memory
+        shmctl(shmid, IPC_RMID, NULL);
 
     return 0;
 }
